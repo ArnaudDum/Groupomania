087650a7c1900ead8352915509b53af8f736dda3
Arnaud Dumoncel <arnaud.dumoncel@gmail.com>
2021-03-15 16:25:21 +0100

    ajout structure API + routes

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
-
 const app = express();
+
+
+// CONNECT DB
+
+const Routes = require('./routes/routes.js');
+const UserRoutes = require('./routes/userRoutes.js');
+
+
+app.use(helmet());
+app.use(bodyParser.json());
+
+// app.use(' CHEMIN API ', Routes);
+// app.use(' CHEMIN API ', UserRoutes);
+
+module.exports = app;
