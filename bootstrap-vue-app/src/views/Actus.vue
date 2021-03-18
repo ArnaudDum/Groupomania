b1615fe2ec9681af8ab4f0058c198b0314c70ecd
Arnaud Dumoncel <arnaud.dumoncel@gmail.com>
2021-03-18 13:33:24 +0100

    ajout page fil d'atualités

--- bootstrap-vue-app/src/views/Actus.vue
+++ bootstrap-vue-app/src/views/Actus.vue
@@ -3,6 +3,16 @@
       
       <Nav/>
 
+      <div class="row">
+        <b-col id="col1" class="col-12 col-md-2 h-100">
+          <h1 class="h2 mt-4">Voici les dernières publications</h1>
+          <p class="mt-3">Vous pouvez réagir et commenter les publications de vos collègues</p>
+        </b-col>
+        <b-col id="col2" class="col-12 col-md-10 h-100">
+          
+        </b-col>
+      </div>
+
   </b-container>
 </template>
 
@@ -17,5 +27,9 @@ export default {
 </script>
 
 <style lang="scss">
-
+#col1 {
+  color: white;
+  position: fixed;
+  background: #1B2D4A;
+}
 </style>
