cd91ec9bc4189fa5d0768ebf575c14f0832ea2df
Arnaud Dumoncel <arnaud.dumoncel@gmail.com>
2021-03-18 16:47:18 +0100

    card component ajouté pour les articles

---
+++ bootstrap-vue-app/src/components/Post.vue
@@ -0,0 +1,17 @@
+<template>
+    <b-card>
+        <b-card-title></b-card-title>
+        <b-card-text></b-card-text>
+        <b-btn>Voir l'article</b-btn>
+    </b-card>
+</template>
+
+<script>
+export default {
+    name: 'Post'
+}
+</script>
+
+<style lang="scss">
+
+</style>
